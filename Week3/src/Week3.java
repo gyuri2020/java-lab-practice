f65e2d8c444dce313097baa93d25edbca52dea10
user <user@DESKTOP-VOVRGCJ>
2021-04-19 18:50:47 +0900

    first commit

---
+++ Week3/src/Week3.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+
+public class Week3 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Enter the first city:");
+		
+		char a1 = (char) System.in.read();
+		while(System.in.read()!='\n') {};
+		
+		System.out.println("Enter the first city:");
+		
+		char a2 = (char) System.in.read();
+		while(System.in.read()!='\n') {};
+		
+		System.out.println(a1+"   " + a2);
+		
+		
+	}
+
+}
